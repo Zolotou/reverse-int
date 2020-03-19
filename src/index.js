42684662c74718894e76576a6eeeb4e2b259344e
Zolotou <vadimkarulin@gmail.com>
2020-03-20 01:40:32 +0300

    feat: make function - reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  let theString = n.toString();
+  let theArray = theString.split("");
+  let theFilter = theArray.filter( item => !(item == "-"));
+  let res = theFilter.reverse().join("");
   
+
+  return res;
+
 }
